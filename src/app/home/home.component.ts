dc24756649b3816a76cfd4823cf657216b5ee17a
Henri Prudhomme <henripru99@gmail.com>
2019-09-20 15:11:44 -0400

    Upload Team Hello World

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <ActionBar title="SnapPong" class="action-bar"></ActionBar>
+    <Image src="https://pngimg.com/uploads/snapchat/snapchat_PNG61.png"></Image>
+  `
+})
+export class HomeComponent {
+  // Your TypeScript logic goes here
+}
