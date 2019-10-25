798cf84b5ace7eb4ddd27091fee6d9f97773374a
fdn2 <fdn2@maroon31.cs.calvin.edu>
2019-10-25 15:26:39 -0400

    cleaned up some things, created a materials folder for database and backend models

---
+++ materials/snapPongBackEndModel/snap.java
@@ -0,0 +1,36 @@
+public class Player{
+  private string mySnapID;
+  private string username;
+  private string bitMoji;
+  private float myElo;
+  private array[] Game gameHistory;
+
+  public string getMySnapID(){
+    return mySnapID;
+  }
+  public string getUsername(){
+    return username;
+  }
+  public string getMyBitmoji(){
+    return bitMoji;
+  }
+  public float getMyElo(){
+    return myElo;
+  }
+
+
+}
+
+public class Game{
+  private int player1score;
+  private int player2score;
+  private string player1ID;
+  private string player2ID;
+
+  public static void main(float score, string 1ID, string2ID){
+    player1ID = 1ID;
+    player2ID = 2ID;
+
+
+  }
+}
