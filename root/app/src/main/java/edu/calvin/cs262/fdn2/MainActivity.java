23ed8931ab2d12cc031bd0f86fdc36917c478e03
dk42 <dk42@gold01.cs.calvin.edu>
2019-10-31 21:24:20 -0400

    added user profile screen to main app

--- root/app/src/main/java/edu/calvin/cs262/fdn2/MainActivity.java
+++ root/app/src/main/java/edu/calvin/cs262/fdn2/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.calvin.cs262.fdn2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.snapchat.kit.sdk.SnapLogin;
 import com.snapchat.kit.sdk.core.controller.LoginStateController; // Import needed for LoginStateControllerd
@@ -10,13 +12,32 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+   /* RecyclerView recyclerview;
+    String[] thescores = {"2-21", "15-21", "21-5", "21-9", "19-21"};
+    String[] thewinrates = {"80", "60", "40", "84", "66"};
+    UserProfileScreenAdapter adapter;*/
+
+       /* setContentView(R.layout.activity_main);
+
+        recyclerview = findViewById(R.id.recyclerView);
+
+        UserProfileScreenAdapter adapter = new UserProfileScreenAdapter(thescores, thewinrates);
+
+        recyclerview.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+
+        recyclerview.setLayoutManager(layoutManager);*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         Button login = findViewById(R.id.login_butt);
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
---
+++ root/app/src/main/java/edu/calvin/cs262/fdn2/UserProfileScreen.java
@@ -0,0 +1,31 @@
+package edu.calvin.cs262.fdn2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class UserProfileScreen extends AppCompatActivity {
+
+    RecyclerView recyclerview;
+    String[] thescores = {"2-21", "15-21", "21-5", "21-9", "19-21"};
+    String[] thewinrates = {"80", "60", "40", "84", "66"};
+    UserProfileScreenAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerview = findViewById(R.id.recyclerView);
+
+        UserProfileScreenAdapter adapter = new UserProfileScreenAdapter(thescores, thewinrates);
+
+        recyclerview.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(UserProfileScreen.this);
+
+        recyclerview.setLayoutManager(layoutManager);
+    }
+}
