e5f1bad444110a4ca8c0997f713558968c7be96e
fdn2 <fdn2@gold12.cs.calvin.edu>
2019-11-21 23:29:50 -0500

    game request functionality

--- SnapPong_V7/root/app/src/main/java/edu/calvin/cs262/fdn2/GameRequestFunctionality.java
+++ SnapPong_V7/root/app/src/main/java/edu/calvin/cs262/fdn2/GameRequestFunctionality.java
@@ -1,6 +1,17 @@
 package edu.calvin.cs262.fdn2;
 import com.snapchat.kit.sdk.SnapCreative;
 import com.snapchat.kit.sdk.creative.api.SnapCreativeKitApi;
+import com.snapchat.kit.sdk.creative.exceptions.SnapMediaSizeException;
+import com.snapchat.kit.sdk.creative.exceptions.SnapStickerSizeException;
+import com.snapchat.kit.sdk.creative.media.SnapMediaFactory;
+import com.snapchat.kit.sdk.creative.media.SnapPhotoFile;
+import com.snapchat.kit.sdk.creative.media.SnapSticker;
+import com.snapchat.kit.sdk.creative.models.SnapPhotoContent;
+
+import java.io.File;
 
 public class GameRequestFunctionality {
+    GameRequestFunctionality(){
+
+    }
 }
--- SnapPong_V7/root/app/src/main/java/edu/calvin/cs262/fdn2/GameRequestScreen.java
+++ SnapPong_V7/root/app/src/main/java/edu/calvin/cs262/fdn2/GameRequestScreen.java
@@ -6,12 +6,40 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import com.snapchat.kit.sdk.creative.exceptions.SnapStickerSizeException;
+import com.snapchat.kit.sdk.creative.media.SnapMediaFactory;
+import com.snapchat.kit.sdk.creative.media.SnapSticker;
+
+import java.io.File;
+
 public class GameRequestScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_request_screen);
+
+        SnapSticker stickerSnap = null;
+        try {
+            File file = new File("/home/Downloads/creativetest.png");
+            stickerSnap = SnapMediaFactory.getSnapStickerFromFile(file);
+        }
+        catch (SnapStickerSizeException e) {
+            handleError(e);
+            return;
+        }
+        // Height and width~~ ~~in pixels
+        stickerSnap.setWidth(300);
+        stickerSnap.setHeight(300);
+
+        // Position is specified as a ratio between 0 & 1 to place the center of the sticker
+        stickerSnap.setPosX(0.5f);
+        stickerSnap.setPosY(0.5f);
+
+        // Specify clockwise rotation desired
+        stickerSnap.setRotationDegreesClockwise(0); // degrees clockwise
+    <your-snap-content>.setSnapSticker(stickerSnap);
+        // Note: Your snap content can be video, photo, or live-camera content
     }
 
     @Override
